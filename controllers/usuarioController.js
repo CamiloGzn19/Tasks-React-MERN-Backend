9734d279081e5592ec3c0e2380717f27541d0074
Camilo Garzón <Camilogzn19@gmail.com>
2022-05-10 17:36:34 -0500

    validation new user info

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -1,16 +1,38 @@
 const Usuario = require("../models/Usuario");
+const bcryptjs = require("bcryptjs");
+const { validationResult } = require("express-validator");
 
 exports.crearUsuario = async (req, res) => {
+  // Revisar si hay errores
+  const errores = validationResult(req);
+  if (!errores.isEmpty()) {
+    return res.status(400).json({ errores: errores.array() });
+  }
+  // Extraer email y password
+  const { email, password } = req.body;
+
   try {
-    let usuario;
+    // Revisar que el usuario sea único
+    let usuario = await Usuario.findOne({
+      email,
+    });
+
+    if (usuario) {
+      return res.status(400).json({ msg: "El usuario ya existe" });
+    }
 
     // Crea el nuevo usuario
     usuario = new Usuario(req.body);
+
+    // Hashear el password
+    const salt = await bcryptjs.genSalt(10);
+    usuario.password = await bcryptjs.hash(password, salt);
+
     // Guarda el nuevo usuario
     await usuario.save();
 
     // Mensaje de confirmación
-    res.send("Usuario creado correctamente");
+    res.json({ msg: "Usuario creado correctamente" });
   } catch (error) {
     console.log(error);
     res.status(400).send("Hubo un error");
