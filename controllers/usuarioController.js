e7e74882dfa2f015d17df1f8f082cfb4ee8b66e4
Camilo Garzón <Camilogzn19@gmail.com>
2022-05-10 17:11:15 -0500

    adding new users to the DB

---
+++ controllers/usuarioController.js
@@ -0,0 +1,18 @@
+const Usuario = require("../models/Usuario");
+
+exports.crearUsuario = async (req, res) => {
+  try {
+    let usuario;
+
+    // Crea el nuevo usuario
+    usuario = new Usuario(req.body);
+    // Guarda el nuevo usuario
+    await usuario.save();
+
+    // Mensaje de confirmación
+    res.send("Usuario creado correctamente");
+  } catch (error) {
+    console.log(error);
+    res.status(400).send("Hubo un error");
+  }
+};
