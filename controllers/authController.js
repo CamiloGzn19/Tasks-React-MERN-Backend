fafec21adc9a454f38da07d78aadf8e67a9121f3
Camilo Garzón <Camilogzn19@gmail.com>
2022-05-13 17:55:18 -0500

    adding authentication to users

---
+++ controllers/authController.js
@@ -0,0 +1,54 @@
+const Usuario = require("../models/Usuario");
+const bcryptjs = require("bcryptjs");
+const { validationResult } = require("express-validator");
+const jwt = require("jsonwebtoken");
+
+exports.autenticarUsuario = async (req, res) => {
+  // Revisar si hay errores
+  const errores = validationResult(req);
+  if (!errores.isEmpty()) {
+    return res.status(400).json({ errores: errores.array() });
+  }
+
+  // Extraer el email y el password
+  const { email, password } = req.body;
+
+  try {
+    // Revisar que sea un usuario registrado
+    let usuario = await Usuario.findOne({ email });
+    if (!usuario) {
+      return res.status(400).json({ msg: "El usuario no existe" });
+    }
+
+    // Revisar el password (Comparación del que viene con la BD)
+    const passCorrecto = await bcryptjs.compare(password, usuario.password);
+    if (!passCorrecto) {
+      return res.status(400).json({ msg: "Password incorrecto" });
+    }
+
+    // Si todo es correcto
+    // Crear y firmar el Json Web Token
+    const payload = {
+      usuario: {
+        id: usuario.id,
+      },
+    };
+
+    // Firmar el Json Web Token
+    jwt.sign(
+      payload,
+      process.env.SECRETA,
+      {
+        expiresIn: 3600, // 1 Hora
+      },
+      (error, token) => {
+        if (error) throw error;
+
+        // Mensaje de confirmación
+        res.json({ token });
+      }
+    );
+  } catch (error) {
+    console.log(error);
+  }
+};
